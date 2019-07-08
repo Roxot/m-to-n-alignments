Fixed neuralibm1 and bernoulli numerical stability issues

alignments/models/neuralibm1.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.functional as F
 import numpy as np
 
+from alignments.constants import epsilon
+
 class NeuralIBM1(nn.Module):
 
     def __init__(self, src_vocab_size, tgt_vocab_size, emb_size, hidden_size, pad_idx):
@@ -44,9 +46,10 @@ class NeuralIBM1(nn.Module):
     def loss(self, p_marginal, y, reduction="mean"):
         p_observed = torch.gather(p_marginal, -1, y.unsqueeze(-1))
         p_observed = p_observed.squeeze(-1)
-        log_likelihood = torch.log(p_observed).sum(dim=1)
+        log_likelihood = torch.log(p_observed + epsilon).sum(dim=1)
 
         loss = -log_likelihood
+
         if reduction == "mean":
             return loss.mean()
         elif reduction == "sum":

alignments/models/vae.py
@@ -147,7 +147,7 @@ class AlignmentVAE(nn.Module):
             if prior_param_1 > 0:
                 # prior_param_1 words per sentence
                 probs = prior_param_1 * (seq_mask_x.float() + epsilon) / (seq_len_x.unsqueeze(-1).float() + 1)
-                probs = torch.clamp(probs, max=(1-epsilon))
+                probs = torch.clamp(probs, max=(1-0.01))
                 probs = probs.unsqueeze(1).repeat(1, seq_mask_y.size(1), 1)
             elif prior_param_2 > 0:
                 # fixed prior_param_2 probability of an alignment